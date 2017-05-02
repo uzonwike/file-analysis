84ee6e609cecb31cfcff24027201840551846d15
Uzo Nwike <uzonwike@gmail.com>
2017-05-02 08:56:51 -0500

    Implemented function to load files

---
+++ loader.py
@@ -0,0 +1,15 @@
+import re
+
+##takes  file and returns a 2-tuple containing a string of the file name, and the file contents
+def load_file(file_name):
+	f = open(file_name)
+	name = file_name
+	contents = f.read()
+	separations = ['\n', '\t']
+	for ch in separations:
+		if ch in contents:
+			contents = contents.replace(ch, ' ')
+	re.sub(r'[^\w]', ' ', contents)
+	return (name, contents)
+
+##print (load_file("months.txt"))
