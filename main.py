9dc0ec6d177e963be987ba377926e7553319bee3
Marcel Champagne <champagn@grinnell.edu>
2017-05-03 09:47:33 -0500

    Working main.py

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from os import listdir
 from os.path import isfile, join
 import file_parser
-from sentiment_classifer import *
+from sentiment_classifier import *
 import database
 
-def load_files(directory):
+def analyze_files(directory):
+	# Get all file paths, load the classifier
 	paths = [f for f in listdir(directory) if isfile(join(directory, f))]
-	classifier = FileClassifer()
+	classifier = FileClassifier()
 
 	# Do analysis for all files in dir
 	for path in paths:
 		# File data is a tuple (name, contents)
-		file_data = load_file(path)
+		file_data = file_parser.load_file(path)
 		if file_data:
 			sentiment = classifier.classify_text(file_data[1])
-			print(sentiment)
-			# Save sentiments results and file data 
-
+			# Save sentiments results and file data to database
