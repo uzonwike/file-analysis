79a36d8363decf5832ad211fed878e6c8eae3682
Larry Boateng Asante <boatenga17@grinnell.edu>
2017-05-06 15:01:00 -0500

    Add all functionalities to predict sentiment

--- preprocess.py
+++ preprocess.py
@@ -4,9 +4,44 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import *
 import string
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-nltk.download('punkt')
-nltk.download('stopwords')
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+
+vectorizer = CountVectorizer()
+tf_transformer = TfidfTransformer()
+
+# Retrieve features from a given sentence
+def extract_features(sentence):
+    if len(sentence) == 0:
+        raise Exception("The sentence passed in is empty")
+    else:
+        return vectorizer.get_feature_names(sentence)
+
+# Transform features to vectors
+def get_feature_vector(sentence):
+    features = extract_features(sentence)
+    feature_count = vectorizer.fit_transform(features)
+    return feature_count
+
+# Get the term-document frequencies
+def term_frequency(sentence):
+    feature_vector = get_feature_vector(sentence)
+    feature_tf = tf_transformer.fit_transform(feature_vector)
+    return feature_tf
+
+# Train the classifier with the term frequencies and targets (i.e positive and negative). This assumes that we pass in the list of indices of the targets. eg. target_idx = [0, 1] ("negative" => 0, "positive" => 1)
+def train_classifier(feature_tf, targets_idx):
+    clf = MultinomialNB().fit(feature_tf, targets_idx)
+    return clf
+
+def predict_sentiment_value(sentence, classifier):
+    new_feature_count = vectorizer.transform(sentence)
+    feature_transform = tf_transformer.transform(new_feature_count)
+    return classifier.predict(feature_transform)
+
+# Returns the predicted sentiment from the target array (i.e targets_list = ["negative", "positive"])
+def get_sentiment_type(predicted_value, targets_arr):
+    return targets_arr[predicted_value];
 
 # Breaks the strings into a list of individual tokens
 def sentence_tokenizer(sentence):
@@ -18,6 +53,10 @@ def sentence_tokenizer(sentence):
         tokens = word_tokenize(remove_punctuations)
         tokens_dict
         return tokens
+
+# Get accuracy score of classifier
+def get_accuracy_score(clf, features, labels):
+    return clf.score(features, labels)
 
 # Creates a word frequency map
 def word_frequency(words):
