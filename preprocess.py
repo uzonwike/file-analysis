f1a6f772e2d05b8ecc8e761512785d4c6f5cf3dd
Larry Boateng Asante <boatenga17@grinnell.edu>
2017-05-08 18:38:45 -0500

    Complete classifier implementation and add datasets

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,39 @@
 import nltk
+import string
+import os, sys
+
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.porter import *
-import string
 
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
-vectorizer = CountVectorizer(min_df=1)
+train_data = []
+train_labels = []
+test_data = []
+test_labels = []
+labels = ['pos', 'neg']
+
+def annotate_files(dir_name):
+
+    # Annotate the train and test data
+    for label in labels:
+        full_path = os.path.join(dir_name, label)
+        file_count = 0
+        total_files = len(os.listdir(full_path))
+
+        for fname in os.listdir(full_path):
+            file_count += 1
+            with open(os.path.join(full_path, fname), 'r') as f:
+                content = f.read()
+                if file_count > (total_files / 3): # Using 1/3 of dataset for testing and remaining for training the model
+                    test_data.append(content)
+                    test_labels.append(label)
+                else:
+                    train_data.append(content)
+                    train_labels.append(label)
 
 # Breaks the strings into a list of individual tokens
 def sentence_tokenizer(sentence):
@@ -32,10 +58,30 @@ def remove_stopwords(sentence):
     filtered = [w for w in tokens if not w in set(stopwords.words('english'))]
     return filtered
 
-# Transform the corpus into vectors matching the occurence of words relative to all the documents
-def vectorize(corpus):
-    vectorizer.fit_transform(corpus)
+# Build vectors from features
+def feature_to_vec(train_data):
+    count_vectorizer = CountVectorizer()
+    feature_train_data_counts = count_vectorizer.fit_transform(train_data)
+    return feature_train_data_counts
 
+# Transform the corpus into vectors matching the occurence of words relative to all the documents
+def vectorize(train_data, test_data):
+    vectorizer = TfidfVectorizer(min_df=5,
+                                 max_df = 0.8,
+                                 sublinear_tf=True,
+                                 use_idf=True)
+    train_vectors = vectorizer.fit_transform(train_data)
+    test_vectors = vectorizer.transform(test_data)
+    return (train_vectors, test_vectors)
+
+def classifier(train_data, test_data):
+    train_test_vectors = vectorize(train_data, test_data)
+    clf = MultinomialNB()
+    clf.fit(train_test_vectors[0], train_labels)
+    print clf.predict(train_test_vectors[1])
+
+annotate_files("./data/test/")
+classifier(train_data, test_data)
 # Creates a word frequency map
 def word_frequency(words):
     if len(words) == 0:
--- sentiment_classifier.py
+++ sentiment_classifier.py
@@ -1,46 +1,44 @@
 import nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
- 
-class FileClassifier:
-  def __init__(self):
-  # Create and train the classifier based on the postive and negative words
-    pos_features = self.get_features_from_file('./data/positive-words.txt', 'positive')
-    neg_features = self.get_features_from_file('./data/negative-words.txt', 'negative')
-    self.model_features = pos_features + neg_features
 
-    training_set = nltk.classify.util.apply_features(self.generate_features, self.model_features)
-    self.classifier = NaiveBayesClassifier.train(training_set)
+# Special formatting for feature
+def word_to_feature(word):
+    return {word: True}
 
-  # Special formatting for feature
-  def word_to_feature(self, word):
-      return {word: True}
+# Get words from list of features
+def get_words(words):
+    all_words = []
+    for (words, sentiment) in words:
+      all_words.extend(words)
+    return all_words
 
-  # Get words from list of features
-  def get_words(self, words):
-      all_words = []
-      for (words, sentiment) in words:
-        all_words.extend(words)
-      return all_words
+# Extract all words as features with a given sentiment from a file
+def get_features_from_file(file_name, sentiment):
+  features = []
+  with open(file_name, 'r') as file:
+    for line in file:
+      word = line.strip()
+      if len(word) >= 3 and len(word) < 12:
+        features.append((word_to_feature(word), sentiment))
+  return features
 
-  # Extract all words as features with a given sentiment from a file
-  def get_features_from_file(self, file_name, sentiment):
-    features = []
-    with open(file_name, 'r') as file:
-      for line in file:
-        word = line.strip()
-        if len(word) >= 3 and len(word) < 12:
-          features.append((self.word_to_feature(word), sentiment))
-    return features
+# Create and train the classifier based on the postive and negative words
+pos_features = get_features_from_file('./data/positive-words.txt', 'positive')
+neg_features = get_features_from_file('./data/negative-words.txt', 'negative')
+model_features = pos_features + neg_features
 
-  # Generate features from text for classifying
-  # Credit to http://www.laurentluce.com/posts/twitter-sentiment-analysis-using-python-and-nltk/
-  def generate_features(self, text):
-    text_words = set(text)
-    text_features = {}
-    for word in self.get_words(self.model_features):
-      text_features['contains('+word+')'] = (word in text_words)
-    return text_features
+# Generate features from text for classifying
+# Credit to http://www.laurentluce.com/posts/twitter-sentiment-analysis-using-python-and-nltk/
+def generate_features(text):
+  text_words = set(text)
+  text_features = {}
+  for word in get_words(model_features):
+    text_features['contains('+word+')'] = (word in text_words)
+  return text_features
 
-  # Classify text
-  def classify_text(self, text):
-    return self.classifier.classify(self.generate_features(text.split()))
+training_set = nltk.classify.util.apply_features(generate_features, model_features)
+classifier = NaiveBayesClassifier.train(training_set)
+
+# Input document to see the result
+text = 'I am an amazing person who loves to eat cake'
+print(classifier.classify(generate_features(text.split())))
