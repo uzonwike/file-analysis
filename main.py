207876fe3cf0f4fe5faf38c39983c623138e66b9
Uzodinma <uzonwike@gmail.com>
2017-05-08 00:30:53 -0500

    start concurrency implementation

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ from os.path import isfile, join
 import file_parser
 from sentiment_classifier import *
 import database
+import threading
+from multiprocessing import Queue
+from multiprocessing.dummy import Pool
 
+directory = "/home/uzonwike/Documents/CSC213/file-analysis/data/"
+paths = [f for f in listdir(directory) if isfile(join(directory, f))]
+classifier = FileClassifier()
+print "YAAAH"
 def analyze_files(directory):
 	# Get all file paths, load the classifier
-	paths = [f for f in listdir(directory) if isfile(join(directory, f))]
-	classifier = FileClassifier()
+	#paths = [f for f in listdir(directory) if isfile(join(directory, f))]
+	#classifier = FileClassifier()
 
 	# Do analysis for all files in dir
 	database.db.connect()
@@ -24,3 +31,46 @@ def analyze_files(directory):
 			database.insert_file(file_results)
 
 	database.db.close()
+
+q = Queue()
+lock = threading.Lock()
+
+#Put all file paths in a queue
+for f in listdir(directory):
+	if isfile(join(directory, f)):
+		q.put(join(directory, f))
+		print f
+#q.join()    #block until all tasks are complete
+
+def do_work(item):
+    with lock:
+        print(item)    #will print file path
+    fh = open (item)
+    src = fh.read()
+
+def worker():
+    while True:
+        item = q.get()
+        try:
+            do_work(item)
+        except Exception as e:
+            print(e)
+        finally:
+            q.task_done()
+
+for i in range(4):
+    t = threading.Thread(target=worker)
+    t.daemon = True
+#    t.start()
+
+#for i in xrange(len(paths)):
+#    paths[i] = directory + "/" + paths[i]
+
+
+# p = Pool() # automatically scales to the number of CPUs available
+# results = p.map(analyze_files("/home/uzonwike/Documents/CSC213/file-analysis/data/"), paths)
+# p.close()
+# p.join()
+
+
+analyze_files("/home/uzonwike/Documents/CSC213/file-analysis/data/")
